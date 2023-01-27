dbf7d2dfadbc3218a058a6eb591e51dc2a55c453
A.YOGUN <yogun@student.42heilbronn.de>
2023-01-27 22:06:33 +0100

    Add files via upload

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 14:27:06 by yogun             #+#    #+#             */
+/*   Updated: 2023/01/27 14:37:39 by yogun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	std::string str;
+	int counter = 1;
+	
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (counter < argc)
+	{
+		str = argv[counter];
+		for (size_t i = 0; i < str.size(); i++)
+			str[i] = std::toupper(str[i]);
+		std::cout << str;
+		counter++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
