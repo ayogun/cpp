cdd4620c7e73810195efc3c50588fab22834b4ff
A.YOGUN <yogun@student.42heilbronn.de>
2023-03-12 21:24:51 +0100

    .

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:31:25 by yogun             #+#    #+#             */
-/*   Updated: 2023/03/07 20:18:14 by yogun            ###   ########.fr       */
+/*   Updated: 2023/03/12 21:14:17 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ Fixed::Fixed() {
 // Constructor with int parameter
 Fixed::Fixed(const int value) {
     std::cout << "Int constructor called" << std::endl;
-    fixed_point_value = value << fractional_bits;
+    fixed_point_value = value << fractional_bits; // shift left to get the fixed point value
 }
 
 // Constructor with float parameter
 Fixed::Fixed(const float value) {
     std::cout << "Float constructor called" << std::endl;
-    fixed_point_value = roundf(value * (1 << fractional_bits));
+    fixed_point_value = roundf(value * (1 << fractional_bits)); // shift left to get the fixed point value and round it to the nearest integer value using roundf from cmath library 
 }
 
 // Destructor
@@ -58,14 +58,14 @@ void Fixed::setRawBits(int const raw) {
 }
 
 float Fixed::toFloat(void) const {
-    return static_cast<float>(fixed_point_value) / (1 << fractional_bits);
+    return static_cast<float>(fixed_point_value) / (1 << fractional_bits); // shift right to get the float value from the fixed point value
 }
 
 int Fixed::toInt(void) const {
-    return fixed_point_value >> fractional_bits;
+    return fixed_point_value >> fractional_bits; // shift right to get the int value from the fixed point value
 }
 
-// overload the insertion operator to output a float representation of the fixed point number
+// overload the insertion operator to output a float representation of the fixed point number to the console using the toFloat() method
 std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
     os << fixed.toFloat();
     return os;
