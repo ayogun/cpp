e375e39cc770e7f50bfa260c227dd472d0d5d390
A.YOGUN <yogun@student.42heilbronn.de>
2023-03-22 20:49:16 +0100

    .

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,25 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 01:15:06 by yogun             #+#    #+#             */
-/*   Updated: 2023/03/22 20:30:28 by yogun            ###   ########.fr       */
+/*   Updated: 2023/03/22 20:40:53 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	return 0;
+	}
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,36 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 01:14:07 by yogun             #+#    #+#             */
-/*   Updated: 2023/03/22 20:31:09 by yogun            ###   ########.fr       */
+/*   Updated: 2023/03/22 20:47:18 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template< typename T > // defines, T is a template. Because we don't know what type we will use. Maybe int, maybe float. God knows...
+
+const T	&max(const T &x ,const T &y) // more const, more better
+{
+	return (x >= y ? x : y);
+}
+
+template< typename U > // defines, U is a template
+
+const U	&min(const U &x ,const U &y) // more const, more better
+{
+	return (x <= y ? x : y);
+}
+
+template< typename V > // defines, V is a template
+
+void swap(V &x, V &y)
+{
+	V tmp = x;
+	x = y;
+	y = tmp;
+}
+
+#endif
