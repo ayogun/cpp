7c01982fdf634edd51d7a38e2cb85a351b5b73c2
byaliego <yogun@student.42heilbronn.de>
2023-04-11 15:36:21 +0200

    .

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -6,23 +6,33 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 01:14:17 by yogun             #+#    #+#             */
-/*   Updated: 2023/03/22 19:27:30 by yogun            ###   ########.fr       */
+/*   Updated: 2023/04/11 15:33:49 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
+/*
+	reinterpret_cast is a type of C++ type casting operator 
+	that allows you to reinterpret the binary representation 
+	of an object of one type as an object of another type without 
+	changing the actual bits of the object. It is the most powerful 
+	and dangerous type casting operator in C++, as it allows you 
+	to bypass type safety checks and can result in undefined behavior 
+	if used improperly.
+*/
+
 // Default constructor
 Serializer::Serializer(void)
 {
-	std::cout << "Default Constructor" << std::endl;
+	//std::cout << "Default Constructor" << std::endl;
 	return ;
 }
 
 // Copy constructor
 Serializer::Serializer(const Serializer &src)
 {
-	std::cout << "Copy Constructor" << std::endl;
+	//std::cout << "Copy Constructor" << std::endl;
 	*this = src;
 	return ;
 }
@@ -30,14 +40,14 @@ Serializer::Serializer(const Serializer &src)
 // Deconstructor
 Serializer::~Serializer(void)
 {
-	std::cout << "Serializer Deconstructor" << std::endl;
+	//std::cout << "Serializer Deconstructor" << std::endl;
 	return ;
 }
 
 // Overloaded Operator
 Serializer &Serializer::operator=(const Serializer &src)
 {
-	std::cout << "Serializer Assignation operator" << std::endl;
+	//std::cout << "Serializer Assignation operator" << std::endl;
 	if (this == &src)
 		return *this;
 
@@ -48,7 +58,7 @@ Serializer &Serializer::operator=(const Serializer &src)
 // Here we cast the uintptr_t to a pointer to Data
 Data	*Serializer::deserialize(uintptr_t raw)
 {
-	return (reinterpret_cast<Data *>(raw));
+	return (reinterpret_cast<Data *>(raw));		// reinterpret_cast is used because we are casting a pointer to a different type
 }
 
 // Here we cast the pointer to a uintptr_t
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 01:15:06 by yogun             #+#    #+#             */
-/*   Updated: 2023/03/22 19:52:58 by yogun            ###   ########.fr       */
+/*   Updated: 2023/04/11 15:31:07 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,63 @@
 
 	int main( void )
 {
+	uintptr_t address;
 
-	Data ptr;
-	ptr.name = "Ali Yigit Ogun";
-	ptr.age = 29;
+	Data human;
+	human.name = "Ali Yigit Ogun";
+	human.age = 29;
 
-	Data ptr2;
-	ptr2.name = "Linus Torvalds";
-	ptr2.age = 62;
+	Data human2;
+	human2.name = "Linus Torvalds";
+	human2.age = 62;
 
 	
+	/*********************** sample *********************************/
 	std::cout << "Original structs:" << std::endl;
-	std::cout << "\taddress ptr: " << &ptr << std::endl;
-	std::cout << "\tname: " << ptr.name << std::endl;
-	std::cout << "\tage: " << ptr.age << std::endl << std::endl;
+	std::cout << "\taddress human: " << &human << std::endl;
+	std::cout << "\tname: " << human.name << std::endl;
+	std::cout << "\tage: " << human.age << std::endl << std::endl;
 
 	
 	std::cout << std::endl; 
 
 	
-	std::cout << "\taddress ptr2: " << &ptr2 << std::endl;
-	std::cout << "\tname: " << ptr2.name << std::endl;
-	std::cout << "\tage: " << ptr2.age << std::endl;
+	std::cout << "\taddress human2: " << &human2 << std::endl;
+	std::cout << "\tname: " << human2.name << std::endl;
+	std::cout << "\tage: " << human2.age << std::endl;
 	std::cout << std::endl ;
 
+	/*********************** sample *********************************/
+
+
+
+	/********************** SERIALIZER *************************/
 	Serializer object;
 
-	Data *reserialized = object.deserialize(object.serialize(&ptr));
+	address = object.serialize(&human);
+
+	std::cout << "Serialized pointer:" << &address << std::endl;
+	/************************ SERIALIZER ***************************/
+
+
+
+	
+	/************************* REVERSE **************************/
+	Data *reserialized = object.deserialize(object.serialize(&human));
+	Data *reserialized2 = object.deserialize(object.serialize(&human2));
 
 	std::cout << "Reserialized structs:" << std::endl;
-	std::cout << "\taddress ptr: " << reserialized << std::endl;
+	std::cout << "\taddress human: " << reserialized << std::endl;
 	std::cout << "\tname: " << reserialized->name << std::endl;
 	std::cout << "\tage: " << reserialized->age << std::endl;
 
 	std::cout << std::endl; 
 	
-	std::cout << "\taddress ptr2: " << &ptr2 << std::endl;
-	std::cout << "\tname: " << ptr2.name << std::endl;
-	std::cout << "\tage: " << ptr2.age << std::endl;
+	std::cout << "\taddress human2: " << &reserialized2 << std::endl;
+	std::cout << "\tname: " << reserialized2->name << std::endl;
+	std::cout << "\tage: " << reserialized2->age << std::endl;
 	std::cout << std::endl; std::cout << std::endl;
+	/************************* REVERSE **************************/
 
 	return (0);
 
